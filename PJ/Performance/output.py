723dd9dcd04b17c36c83bab3a60bc058999f2a02
szcghost <1466928388@qq.com>
2017-09-01 17:08:30 +0800

    增加图的函数(store_graph)

--- PJ/Performance/output.py
+++ PJ/Performance/output.py
@@ -4,19 +4,28 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import datetime as dt
+
+
 def store_position(all_tradedate_position, id, para_dict):
-    path = './Backtest_Position/'+ id +'.txt'
     all_tradedate_position = all_tradedate_position.copy()
+    path = './Backtest_result/Position/' + id + '.txt'
     df = all_tradedate_position.loc[:, ['time', 'stkcd', 'weight']]
     df.to_csv(path, sep='\t', index=True)
     f = open(path, 'r+')
-    lines = f.readlines()
-    for k, v in para_dict.items():
-        lines.insert(0, '{}:{} \n'.format(k, v))
+    lines1 = f.readlines()
+    lines2 = ['{}: {}\n'.format(k, v) for k, v in para_dict.items()]
+    lines2.insert(0, "INPUT:\n")
+    lines2.append('\n')
+    lines1.insert(0, "OUTPUT:\n")
+    lines2.extend(lines1)
     f.seek(0, 0)
-    f.writelines(lines)
+    f.writelines(lines2)
     f.close()
 
-def store_graph():
-
-    pass
+def store_graph(portfolio, id, para_dict):
+    portfolio = portfolio.copy()
+    path = './Backtest_result/Graph/' + id + '.jpg'
+    portfolio['time'] = [dt.datetime.strptime(i, '%Y-%m-%d') for i in portfolio.time]
+    plt.plot(portfolio.time, portfolio.value_hedged)
+    plt.savefig(path)
